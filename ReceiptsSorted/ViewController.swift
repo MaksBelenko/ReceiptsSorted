e72a2aa616af417578247e63ba2632f14b46b800
“MaksBelenko” <maksim.belenko@gmail.com>
2019-12-16 22:10:38 +0000

    Added camera functionality: taking pictures, selecting from library and saving to the library

--- ReceiptsSorted/ViewController.swift
+++ ReceiptsSorted/ViewController.swift
@@ -8,13 +8,105 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum ImageSource {
+    case photoLibrary
+    case camera
+}
+
+class ViewController: UIViewController, UINavigationControllerDelegate  {
+
+    @IBOutlet var imageTake: UIImageView!
+    var imagePicker: UIImagePickerController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    //MARK: - Select image from library
+    @IBAction func selectPhotoFromLibrary(_ sender: UIButton) {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            showAlertWith(title: "Photo Library is unavailable!", message: "You did not allow access to your photo library")
+            return
+        }
+        
+        selectImageFrom(.photoLibrary)
+    }
+    
+    func selectImageFrom(_ source: ImageSource){
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        switch source {
+        case .camera:
+            imagePicker.sourceType = .camera
+        case .photoLibrary:
+            imagePicker.sourceType = .photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    //MARK: - Take a picture on camera
+    @IBAction func takePhoto(_ sender: UIButton) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            showAlertWith(title: "Camera is unavailable!", message: "You did not allow access to your camera or the camera is broken")
+            return
+        }
+        
+        selectImageFrom(.camera)
+    }
+    
+    
+    
+    //MARK: - Saving Image here
+    @IBAction func save(_ sender: Any) {
+        guard let selectedImage = imageTake.image else {
+            print("Image not found!")
+            showAlertWith(title: "No image selected!", message: "")
+            return
+        }
+        UIImageWriteToSavedPhotosAlbum(selectedImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    
+    
+    
+    //MARK: - Add image to Library
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            showAlertWith(title: "Save error", message: error.localizedDescription)
+        } else {
+            showAlertWith(title: "Saved!", message: "Your image has been saved to your photos.")
+        }
+    }
 
+    func showAlertWith(title: String, message: String){
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+ }
+
+
+
+
+
+
+
+
+//MARK: - Extension for ImagerPicker
+ extension ViewController: UIImagePickerControllerDelegate{
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        imagePicker.dismiss(animated: true, completion: nil)
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            print("Image not found!")
+            return
+        }
+        imageTake.image = selectedImage
+    }
 }
+
 
