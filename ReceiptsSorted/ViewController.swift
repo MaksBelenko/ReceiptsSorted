f51767a2035839e7b0efee384d059d02a7c370be
“MaksBelenko” <maksim.belenko@gmail.com>
2019-12-16 21:27:08 +0000

    Initial Commit

---
+++ ReceiptsSorted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReceiptsSorted
+//
+//  Created by Maksim on 16/12/2019.
+//  Copyright © 2019 Maksim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
